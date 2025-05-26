0b024a2db1b8ed52b47d4bade441e3d8ee75a333
riccardo-guido <guidoriccardo02@gmail.com>
2025-05-26 16:20:39 +0200

    form-created

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,32 @@
-// import { useState } from "react";
+import { useState } from "react";
 
 
 export default function Main() {
-  const articleslist = ["Articolo 1", "Articolo 2", "Articolo 3"];
+  const [articleslist, setArticleList] = useState(["Articolo 1", "Articolo 2", "Articolo 3"]);
+  const [newArticole, setNewArticle] = useState("")
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
 
+    const newArticleList = [...articleslist, newArticole];
+    setArticleList(newArticleList)
+     
+
+  }
   return(
     <main>
-      <div className="container">
+      <div className="container mt-5">
+        <form onSubmit={handleFormSubmit} className="d-flex">
+          <input 
+          value={newArticole}
+          onChange={(event) => setNewArticle(event.target.value)}
+
+          className="form-control" type="text" />
+          <button className="btn btn-primary">Aggiungi</button>
+
+        </form>
+
+
+
         <ul>
             {articleslist.map((article, index) => (
               <li key={index}> {article}</li>
